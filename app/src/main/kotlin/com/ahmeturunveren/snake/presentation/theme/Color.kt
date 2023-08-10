7ee3ee5213b2820c23a604880c8a8c36172b1413
Ahmet URUNVEREN <urunverenahmett@gmail.com>
2023-08-10 23:32:43 +0300

    Snake is completed.

---
+++ app/src/main/kotlin/com/ahmeturunveren/snake/presentation/theme/Color.kt
@@ -0,0 +1,6 @@
+package com.ahmeturunveren.snake.presentation.theme
+
+import androidx.compose.ui.graphics.Color
+
+val LightGreen = Color(0xFFFFEB3B)
+val DarkGreen = Color(0xFF2D321D)
---
+++ app/src/main/kotlin/com/ahmeturunveren/snake/presentation/theme/Dimen.kt
@@ -0,0 +1,12 @@
+package com.ahmeturunveren.snake.presentation.theme
+
+import androidx.compose.ui.unit.dp
+
+val padding8dp = 6.dp
+val padding16dp = 14.dp
+val padding24dp = 22.dp
+val padding64dp = 60.dp
+val width248dp = 240.dp
+val border2dp = 4.dp
+val corner4dp = 6.dp
+val size64dp = 60.dp
